f2a52e46bad4adc97453b3714a3fde754114db4e
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-05-04 21:50:23 +0530

    UP;Google Maps API Client

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -2,3 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const { googleMapsClient } = require('@google/maps');
 const router = express.Router();
+
+//Google Maps API Client
+const googleMaps = googleMapsClient({
+    key: process.env.GOOGLE_MAPS_API_KEY, 
+});
